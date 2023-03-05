df7a0b7b2bf2a9180b2ae10784d909ca591e78bd
abbas <abbashesenli19@gmail.com>
2023-03-05 16:17:00 +0400

    lesson 4 hello world

---
+++ src/main/java/Homeworks/lesson4/Lesson4.java
@@ -0,0 +1,14 @@
+package Homeworks.lesson4;
+
+
+public class Lesson4 {
+    public static void main(String[] args) {
+
+        System.out.print("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.printf("Hello,%s","World");
+        System.out.printf("HelloWorld:%.2f", 50F);
+    }
+     }
+
+
