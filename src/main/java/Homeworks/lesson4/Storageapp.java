dc3e3e357722c18a772dea72197537ac726e2844
abbas <abbashesenli19@gmail.com>
2023-03-06 12:07:49 +0400

    lesson 4

---
+++ src/main/java/Homeworks/lesson4/Storageapp.java
@@ -0,0 +1,21 @@
+package Homeworks.lesson4;
+
+public class Storageapp {
+    public static void main(String[] args) {
+        System.out.println("abbas");
+        System.out.println("abbas");
+        System.out.println("abbas");
+        System.out.println("abbas");
+        System.out.println("abbas");
+        System.out.println("abbas");
+        System.out.println("abbas");
+        System.out.println("abbas");
+        System.out.println("abbas");
+        System.out.println("abbas");
+        System.out.println("abbas");
+        System.out.println("abbas");
+        System.out.println("abbas");
+
+
+    }
+}
---
+++ src/main/java/Homeworks/lesson4/primitivetypes.java
@@ -0,0 +1,7 @@
+package Homeworks.lesson4;
+
+public class primitivetypes {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
