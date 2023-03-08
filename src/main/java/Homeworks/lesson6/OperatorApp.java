5b5c09a3326ea8d5093dbb3262e9fa94d93da2fa
abbas <abbashesenli19@gmail.com>
2023-03-08 18:25:27 +0400

    lesson 5 operators

---
+++ src/main/java/Homeworks/lesson6/OperatorApp.java
@@ -0,0 +1,11 @@
+package Homeworks.lesson6;
+
+public class OperatorApp {
+    public static void main(String[] args) {
+        int a = 7;
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+
+    }
+}
