dd9c16e6b5412871f6eb5e079ed70a4b9da7dd30
abbas <abbashesenli19@gmail.com>
2023-03-10 10:55:01 +0400

    lesson

---
+++ src/main/java/homeworks/lesson7/Alphabet.java
@@ -0,0 +1,13 @@
+package homeworks.lesson7;
+
+import java.util.Scanner;
+
+public class Alphabet {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 91; i++) {
+            System.out.println((char)i);
+
+        }
+    }
+}
