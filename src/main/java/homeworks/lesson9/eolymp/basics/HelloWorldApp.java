6da53f6ca966aba4c4b584168527a188ce99a76d
abbas <abbashesenli19@gmail.com>
2023-03-14 00:03:03 +0400

    hello world

--- src/main/java/homeworks/lesson9/eolymp/basics/HelloWorldApp.java
+++ src/main/java/homeworks/lesson9/eolymp/basics/HelloWorldApp.java
@@ -1,6 +1,6 @@
 package homeworks.lesson9.eolymp.basics;
 
-public class HelloWorldApps {
+public class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
