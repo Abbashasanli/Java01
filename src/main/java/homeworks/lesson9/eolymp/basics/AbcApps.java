499fc6f11fa62c0cd2a0ea8b5993ae98f0880657
abbas <abbashesenli19@gmail.com>
2023-03-13 23:55:42 +0400

    abc

--- src/main/java/homeworks/lesson9/eolymp/basics/AbcApps.java
+++ src/main/java/homeworks/lesson9/eolymp/basics/AbcApps.java
@@ -1,6 +1,6 @@
 package homeworks.lesson9.eolymp.basics;
 
-public class AbcApp {
+public class AbcApps {
     public static void main(String[] args) {
         System.out.println("a");
         System.out.println("bb");
