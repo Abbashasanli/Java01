f59dbf61543ab0805ebbbaf792bc47eaf4713baa
abbas <abbashesenli19@gmail.com>
2023-03-13 22:26:47 +0400

    abc

---
+++ src/main/java/homeworks/lesson9/eolymp/AbcApp.java
@@ -0,0 +1,9 @@
+package homeworks.lesson9.eolymp;
+
+public class AbcApp {
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println("bb");
+        System.out.println("ccc");
+    }
+}
