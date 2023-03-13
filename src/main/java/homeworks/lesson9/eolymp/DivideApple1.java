9bf7c4d7b412f9fcc356a6b408c8c14ed79c41b2
abbas <abbashesenli19@gmail.com>
2023-03-13 22:18:41 +0400

    divide apples 1

---
+++ src/main/java/homeworks/lesson9/eolymp/DivideApple1.java
@@ -0,0 +1,15 @@
+package homeworks.lesson9.eolymp;
+
+import java.util.Scanner;
+
+public class DivideApple1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        System.out.println(k / n);
+
+    }
+}
+
+
