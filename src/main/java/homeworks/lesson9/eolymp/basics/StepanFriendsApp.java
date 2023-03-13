dc8eed457e7e3f5e51b3388e2127e0303035b745
abbas <abbashesenli19@gmail.com>
2023-03-14 00:10:33 +0400

    stepan friends

--- src/main/java/homeworks/lesson9/eolymp/basics/StepanFriendsApp.java
+++ src/main/java/homeworks/lesson9/eolymp/basics/StepanFriendsApp.java
@@ -2,7 +2,7 @@ package homeworks.lesson9.eolymp.basics;
 
 import java.util.Scanner;
 
-public class StepanFriendsApps {
+public class StepanFriendsApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
