f9fa5569ba9537f9a2ea327b8cdc07a14114e3c7
abbas <abbashesenli19@gmail.com>
2023-03-13 23:58:20 +0400

    area of rectangle

--- src/main/java/homeworks/lesson9/eolymp/basics/AreaApp.java
+++ src/main/java/homeworks/lesson9/eolymp/basics/AreaApp.java
@@ -2,7 +2,7 @@ package homeworks.lesson9.eolymp.basics;
 
 import java.util.Scanner;
 
-public class AreaApps {
+public class AreaApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
