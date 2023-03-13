80f1424db23e2281d6e996e5c4924611f3f6667e
abbas <abbashesenli19@gmail.com>
2023-03-14 00:01:38 +0400

    hello phyton

--- src/main/java/homeworks/lesson9/eolymp/basics/HelloPhytonApps.java
+++ src/main/java/homeworks/lesson9/eolymp/basics/HelloPhytonApps.java
@@ -1,6 +1,6 @@
 package homeworks.lesson9.eolymp.basics;
 
-public class HelloPhytonApp {
+public class HelloPhytonApps {
     public static void main(String[] args) {
         System.out.println("Hello, Python!");
     }
