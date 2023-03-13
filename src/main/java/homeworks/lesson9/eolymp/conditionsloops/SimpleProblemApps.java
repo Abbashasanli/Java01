6f723498e5c18b4fad48d16efabb60f9c5a8bb08
abbas <abbashesenli19@gmail.com>
2023-03-14 00:12:18 +0400

    simple problem

--- src/main/java/homeworks/lesson9/eolymp/conditionsloops/SimpleProblemApps.java
+++ src/main/java/homeworks/lesson9/eolymp/conditionsloops/SimpleProblemApps.java
@@ -2,7 +2,7 @@ package homeworks.lesson9.eolymp.conditionsloops;
 
 import java.util.Scanner;
 
-public class SimpleProblemApp {
+public class SimpleProblemApps {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
