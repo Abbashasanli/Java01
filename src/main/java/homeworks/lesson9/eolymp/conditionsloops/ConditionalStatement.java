021171ccc7d40b298a4ebec16421b2b2964d84c4
abbas <abbashesenli19@gmail.com>
2023-03-15 12:56:32 +0400

    conditional statement 1

---
+++ src/main/java/homeworks/lesson9/eolymp/conditionsloops/ConditionalStatement.java
@@ -0,0 +1,14 @@
+package homeworks.lesson9.eolymp.conditionsloops;
+
+import java.util.Scanner;
+
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int a = new Scanner(System.in).nextInt();
+        if (a >= 5) {
+            System.out.println(a + 7);
+        } else {
+            System.out.println(a * a - 3 * a + 4);
+        }
+    }
+}
