bc951d4d2ee682d9fa3ec950232392cf6ec09dff
abbas <abbashesenli19@gmail.com>
2023-03-31 12:20:38 +0400

    dollars ine the bank

--- src/main/java/homeworks/lesson11/eolymp/SumofSquaress.java
+++ src/main/java/homeworks/lesson11/eolymp/SumofSquaress.java
@@ -2,10 +2,9 @@ package homeworks.lesson11.eolymp;
 
 import java.util.Scanner;
 
-public class SumofSquares {
+public class SumofSquaress {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
+        int a = new Scanner(System.in).nextInt();
         int sum = 0;
         for (int i = 1; i <= a; i++) {
             sum += i * i;
