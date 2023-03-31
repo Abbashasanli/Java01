ec466197ae2fb6f26e08da8edf72f1f5d954fa2a
abbas <abbashesenli19@gmail.com>
2023-03-31 20:51:09 +0400

    maximum salary

---
+++ src/main/java/homeworks/lesson15/MaxSalary.java
@@ -0,0 +1,21 @@
+package homeworks.lesson15;
+
+import java.util.Scanner;
+
+public class MaxSalary {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        double max = 0;
+        for (int i = 0; i < num; i++) {
+            double b = scan.nextDouble();
+            if (Math.abs(b) > max) {
+                max = Math.abs(b);
+            }
+
+
+        }
+        System.out.printf("%.2f", max);
+    }
+}
+
