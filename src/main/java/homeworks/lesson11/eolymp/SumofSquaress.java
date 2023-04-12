6d29477278bdccc5fbdd7bc5add141a5ce5c593b
abbas <abbashesenli19@gmail.com>
2023-04-12 20:49:39 +0400

    changes

--- src/main/java/homeworks/lesson11/eolymp/SumofSquaress.java
+++ src/main/java/homeworks/lesson11/eolymp/SumofSquaress.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class SumofSquaress {
     public static void main(String[] args) {
-        int a = new Scanner(System.in).nextInt();
+        Scanner scan = new Scanner(System.in);
+        int a= scan.nextInt();
         int sum = 0;
         for (int i = 1; i <= a; i++) {
             sum += i * i;
         }
-        System.out.println(sum);
+        System.out.print(sum);
     }
 }
