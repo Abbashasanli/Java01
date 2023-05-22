5c7a5c5d055feedc201efc7b004d1aeb0a028956
abbas <abbashesenli19@gmail.com>
2023-05-22 19:33:22 +0400

    StreamApp

---
+++ src/main/java/homeworks/lesson37/StreamApp.java
@@ -0,0 +1,21 @@
+package homeworks.lesson37;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class StreamApp {
+    public static void main(String[] args) {
+        List<Integer>evenNum= new Random().ints(20,1,50)
+                .filter(num-> num%2==0)
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println(evenNum);
+        Set<Integer> set = evenNum.stream().filter(num -> num > 10 && num < 30).collect(Collectors.toSet());
+        System.out.println(set);
+
+
+    }
+
+}
