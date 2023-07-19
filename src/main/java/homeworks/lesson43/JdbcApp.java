436c228ba432a23f5f9614b65b1e2e2e69ba4053
abbas <abbashesenli19@gmail.com>
2023-07-19 12:16:59 +0400

    lesson 42

--- src/main/java/homeworks/lesson43/JdbcApp.java
+++ src/main/java/homeworks/lesson43/JdbcApp.java
@@ -7,8 +7,17 @@ import java.util.List;
 
 public class JdbcApp {
     public static final String getAllBooksSql = "SELECT * FROM books;";
+    public static final String updateAuthor ="Update books set author=?  where  author =?";
     public static void main(String[] args) {
+        updateAuthor("Ilaq","Arthur Conan Doyle");
         getBooks().stream().forEach(System.out::println);
+
+
+
+
+
+
+
 
 
 }
@@ -33,4 +42,22 @@ public class JdbcApp {
             }
             return Collections.emptyList();
         }
+
+        private static void updateAuthor(String name,String author ) {
+            try(Connection conn = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/lesson27",
+                    "postgres",
+                    "mysecretpassword")) {
+                PreparedStatement query = conn.prepareStatement(updateAuthor);
+                 query.setString(1,name);
+                 query.setString(2,author);
+                int i = query.executeUpdate();
+                System.out.println(i);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 }
+
+
+
